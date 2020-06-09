a950b8909369f8e6697e0b56ce14d826d82e305d
Annnh3 <anhknguyen97@yahoo.com>
2020-06-08 19:31:18 -0700

    Implement run method and call initializeRoutes

--- app.go
+++ app.go
@@ -27,9 +27,13 @@ func (a *App) Initialize(user, password, dbname string) {
 	}
 
 	a.Router = mux.NewRouter()
+
+	a.initializeRoutes()
 }
 
-func (a *App) Run(addr string) {}
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(":8010", a.Router))
+}
 
 func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
