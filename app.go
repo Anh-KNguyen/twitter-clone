23ff7fe42a5bfb664018ba35df05fc3f848e30c5
Annnh3 <anhknguyen97@yahoo.com>
2020-06-08 19:09:35 -0700

    Define the routes for handlers

--- app.go
+++ app.go
@@ -128,5 +128,9 @@ func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *App) initializeRoutes() {
-
+	a.Router.HandleFunc("/products", a.getProducts).Methods("GET")
+	a.Router.HandleFunc("/product", a.createProduct).Methods("POST")
+	a.Router.HandleFunc("/product/{id:[0-9]+}", a.getProduct).Methods("GET")
+	a.Router.HandleFunc("/product/{id:[0-9]+}", a.updateProduct).Methods("PUT")
+	a.Router.HandleFunc("/product/{id:[0-9]+}", a.deleteProduct).Methods("DELETE")
 }
